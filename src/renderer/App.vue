1088aa4dba09c65817defcfde91ce3b5fd132142
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2018-03-15 23:58:07 -0500

    First Commit :pineapple: :bear:

---
+++ src/renderer/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div style="-webkit-app-region: drag; width: 100%; height: 35px;">
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Shutte'
+  }
+</script>
+
+<style lang="scss">
+  $primary-color: #029FDD;
+  @import "node_modules/spectre.css/src/spectre";
+  @import "node_modules/spectre.css/src/spectre-exp";
+  @import "node_modules/spectre.css/src/spectre-icons";
+
+  #container {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+  }
+
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+
+  <!-- Header with logo and app name -->
+  <div id="headerContainer" class="container">
+    <img class="centered" id="logo" src="~@/assets/logo.png" alt="electron-vue" />
+    <h1 id="appName"><strong>Shuttle</strong></h1>
+  </div>
+
+  <!-- Empty Container, shown when the user has no recent projects -->
+  <div id="emptyContainer" class="container">
+      <div class="columns">
+        <div class="column col-7 col-mx-auto">
+          <div class="empty">
+            
+            <h4><small>No recent projects</small></h4>
+            <br><br>
+            <div class="columns">
+              <div class="column">
+                <div class="empty-icon">
+                  <i class="icon icon-2x icon-plus"></i>
+                </div>
+                <button class="btn btn-link">Create a Project</button>
+              </div>
+              <div class="divider-vert"></div>
+              <div class="column">
+                <div class="empty-icon">
+                  <i class="icon icon-2x icon-share"></i>
+                </div>
+                <button class="btn btn-link">Open existing project...</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+</div>
+</template>
+
+<style>
+
+  #headerContainer {
+    margin-top: 60px;
+  }
+
+  #appName {
+    text-align: center;
+  }
+
+  #emptyContainer {
+    margin-top: 60px;
+  }
+
+  #logo {
+    height: 150px;
+    margin-bottom: 20px;
+    width: auto;
+  }
+</style>
