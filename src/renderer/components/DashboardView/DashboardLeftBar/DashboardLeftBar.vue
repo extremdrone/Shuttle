76f1b20c63a595a829d0276f50251e587c69c8ba
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2018-03-28 12:58:36 -0500

    Working in View Hierarchy

--- src/renderer/components/DashboardView/DashboardLeftBar/DashboardLeftBar.vue
+++ src/renderer/components/DashboardView/DashboardLeftBar/DashboardLeftBar.vue
@@ -1,6 +1,59 @@
 <template>
     <div id="leftBar">
-
+        <div class="columns">
+            <div id="viewHierarchyContainer" class="column col-12">
+              <small>View Hierarchy</small>
+              <div class="divider"></div>
+              <div class="accordion">
+                <input type="checkbox" id="accordion-4" name="accordion-checkbox" hidden="" checked="">
+                <label class="accordion-header c-hand" for="accordion-4">
+                  Elements
+                </label>
+                <div class="accordion-body">
+                  <ul class="menu menu-nav">
+                    <li class="menu-item">
+                      <a href="#accordions">Element 1</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="#accordions">Element 2</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="accordion">
+                <input type="checkbox" id="accordion-5" name="accordion-checkbox" hidden="">
+                <label class="accordion-header c-hand" for="accordion-5">
+                  Layout
+                </label>
+                <div class="accordion-body">
+                  <ul class="menu menu-nav">
+                    <li class="menu-item">
+                      <a href="#accordions">Layout 1</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="#accordions">Layout 2</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="accordion">
+                <input type="checkbox" id="accordion-6" name="accordion-checkbox" hidden="">
+                <label class="accordion-header c-hand" for="accordion-6">
+                  Components
+                </label>
+                <div class="accordion-body">
+                  <ul class="menu menu-nav">
+                    <li class="menu-item">
+                      <a href="#accordions">Component 1</a>
+                    </li>
+                    <li class="menu-item">
+                      <a href="#accordions">Component 2</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -17,5 +70,11 @@
         width: 250px;
         bottom: 0px;
         border-right: 1px solid #eaeaea;
+    }
+
+    #viewHierarchyContainer {
+      padding-left: 20px;
+      padding-top: 20px;
+      padding-left: 20px;
     }
 </style>
--- src/renderer/components/DashboardView/DashboardTopBar/DashboardTopBar.vue
+++ src/renderer/components/DashboardView/DashboardTopBar/DashboardTopBar.vue
@@ -2,11 +2,15 @@
     <div>
         <header id="navbar" class="navbar">
             <section class="navbar-section">
+                <a href="#" class="btn btn-link">Undo</a>
+                <a href="#" class="btn btn-link">Redo</a>
+                <a href="#" class="btn btn-link">Save</a>
             </section>
             <section class="navbar-center">
                 <img class="centered" id="logo" src="~@/assets/logo.png" alt="electron-vue"/>
             </section>
             <section class="navbar-section">
+                <a href="#" class="btn btn-link">Settings</a>
                 <a href="#" class="btn btn-link">Run</a>
                 <a href="#" class="btn btn-link">Share</a>
             </section>
